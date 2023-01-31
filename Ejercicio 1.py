e101350dbd46f0b652b97363d6254b0c18561c90
Rosa Reyna <rreynarive1@educacion.navarra.es>
2023-01-31 15:51:43 +0100

    repasar ejer 1

---
+++ Ejercicio 1.py
@@ -0,0 +1,48 @@
+class Producto:
+
+    def __init__(self, codigo, nombre, precio):
+        self.__codigo = codigo
+        self.__nombre = nombre
+        self.__precio = precio
+
+    @property
+    def codigo(self):
+        return self.__codigo
+
+    @codigo.setter
+    def nombre(self, valor):
+        self.__nombre = valor
+
+    @property
+    def nombre(self):
+        return self.__nombre
+
+    @nombre.setter
+    def nombre(self, valor):
+        self.__codigo = valor
+
+    @property
+    def precio(self):
+        return self.__codigo
+
+    @precio.setter
+    def precio(self, valor):
+        self.__precio = valor
+
+    def calcular_total(self, unidades):
+        return self.precio * unidades
+
+    def __str__(self):
+        return "codigo: " + str(self.__codigo) + " nombre: " + self.__nombre + " precio: " + str(self.__precio)
+
+p1 = Producto(1, "Producto 1", 5)
+p2 = Producto(2, "Producto 2", 10)
+p3 = Producto(3, "Producto 3", 20)
+
+print(p1)
+print(p2)
+print(p3)
+
+print(p1.calcular_total(5))
+print(p2.calcular_total(5))
+print(p3.calcular_total(5))
